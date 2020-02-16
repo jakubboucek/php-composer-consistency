7ceb5547d2b8f4931b2c47e9ecb1125a766d273d
Jakub Bouček <pan@jakubboucek.cz>
2020-02-16 22:11:39 +0100

    Add manual loader for manual installation

---
+++ loader.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/src/FileReadException.php';
+require __DIR__ . '/src/ComposerInconsitencyException.php';
+require __DIR__ . '/src/Checker.php';
